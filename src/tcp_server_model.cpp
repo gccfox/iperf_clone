2cfd30d524bbeb5af2ba4419dd123f9945e12eb8
Kontrurabo <kontrurabo@localhost.localdomain>
2014-03-31 17:21:24 +0400

    Fixed: code is more readable now.

--- src/tcp_server_model.cpp
+++ src/tcp_server_model.cpp
@@ -1,7 +1,6 @@
 #include "tcp_server_model.h"
 //structure with data
 struct msg{
-//	int all;//number of datapockets
 	int that;//count of current datapocket
 	char self;//data
 	int size;//size of datapocket
@@ -30,84 +29,120 @@ void TcpServerModel::createTcpSocket(int &lis,struct sockaddr_in add) {
 	if ( bind (lis , (struct sockaddr *) & add , sizeof (add) ) < 0){
 		perror( "binding error\n" ); //binding of socket and seeking for mistake
     	exit(2);
-	}
-	else
+	}else{
 		printf("Succesfull binding\n");
+		}
 }
 
 
-void TcpServerModel::acceptTcp(int lis,int &s, struct msg b)
+void TcpServerModel::acceptClient(int lis,int &s)
+{
+	s = accept(lis, NULL, NULL);
+                printf("Try");
+                if(s < 0){
+                        perror("accepting error\n");
+                        exit(3);
+                }
+                        else
+                                printf("Succesfull accepting\n");
+
+                        printf("Accepting friendly forces\n");
+
+}
+
+
+void TcpServerModel::receiveInitData(long int &number_of_pockets,int &s, struct msg b){
+int bytes_read;
+bytes_read = recv(s,(void *)&number_of_pockets, sizeof(int), 0);
+if(bytes_read <= 0){
+		printf("Error in reading initial data\n");
+        	   }else{
+	        printf("Successful received initial pocket\n");
+                        }
+}
+
+void TcpServerModel::printSpeed(unsigned long int time, long int number_of_pockets)
+{
+	 	unsigned int kb_speed = (number_of_pockets * sizeof(struct msg));
+                kb_speed = kb_speed/1024;
+		kb_speed = kb_speed/time;
+                printf("Скорость - %d kbyte/s\n",kb_speed);
+}
+
+void TcpServerModel::printTimeInSec(unsigned long int time)
+{
+	 printf("Время работы в секундах:\n");
+                printf("%d\n",time);
+
+}
+
+
+void TcpServerModel::printNumberOfPockets(unsigned long int count ,unsigned long int number)
+{
+	printf("Пакетов %d из %d получено\n",count,number);
+	printf("Не дошло: %d\n", (number - count));
+	unsigned long int proc = (number - count);
+	printf("Процент потерь: %d %\n",proc/number);
+}
+
+void TcpServerModel::printStatistic(unsigned long int time, long int number_of_pockets,unsigned long int count)
+{
+	TcpServerModel::printSpeed(time,number_of_pockets);
+	TcpServerModel::printTimeInSec(time);
+	TcpServerModel::printNumberOfPockets(count ,number_of_pockets);
+}
+void TcpServerModel::acceptTcp(int lis,int &s, struct msg structure_to_write)
 {
 
 	int bytes_read;
 	struct timespec time1;
 	struct timespec time2;
 	unsigned long int hope;
-	   while(1){//cyckle of listening  and accepting
+	   while(1){                       //cyckle of listening  and accepting
  	  	clock_gettime(0, &time1);
-	 	long int k;
-		//printf("%d\n", time1.tv_nsec);
-		k = 0;
-		s = accept(lis, NULL, NULL);
-        	printf("Try");
-		if(s < 0){
-            		perror("accepting error\n");
-            		exit(3);
-        	}
-			else
-				printf("Succesfull accepting\n");
+	 	long int count_of_pockets;
+		count_of_pockets = 0;
+		long int number_of_pockets;
+		number_of_pockets = 0;
+		
+		TcpServerModel::acceptClient(lis, s);
+		
+		printf("Reading...\n");
+                printf("------------\n");
 
-			printf("Accepting friendly forces\n");
-			printf("Reading...\n");
-			printf("------------\n");
-		long int g;
-		g=0;	
-			bytes_read = recv(s,(void *)&g, sizeof(int), 0);
+		 TcpServerModel::receiveInitData(number_of_pockets, s, structure_to_write); 
 
-                                if(bytes_read <= 0){
-                                        //printf("End of Reading!\n");
-					break;
-                                }
-                                else{
-                                  	//k = k+1;
-					clock_gettime(0, &time2);
-					clock_gettime(0, &time1);
-					printf("%d\n", time2.tv_nsec);
-					         }
+			clock_gettime(0, &time2);
+			clock_gettime(0, &time1);
 
-        	
-			for(int i=0; i<g; i++){ //cyckle of reading of datapockets
-            	bytes_read = recv(s,(void *)&b, sizeof(b), 0);
+		                	
+			for(int i=0; i<number_of_pockets; i++){ //cyckle of reading of datapockets
+           
+
+				
+				bytes_read = recv(s,(void *)&structure_to_write, sizeof(structure_to_write), 0);
             		
 				if(bytes_read <= 0){	
 					printf("End of Reading!\n");
-					//k = k-1; 
-					//clock_gettime(3,&time2);
 					}
 				else{
-					k = k+1;
-					//printf("%d : %d %d %d\n", ntohs(b.that), b.self, b.size, g);	
+					count_of_pockets = count_of_pockets+1;
 					clock_gettime(0,&time2);
-					hope =  time2.tv_sec - time1.tv_sec;
-					printf("%d\n", hope);
-                                        printf("%d\n", time1.tv_nsec);
-					//printf("iteration -%d\n", k);
-                                        	
+					hope =  time2.tv_sec - time1.tv_sec;                              	
 				}
 			}
 		clock_gettime(0,&time2);
-		printf("Время работы в секундах:\n");
-	 	printf("%d\n",hope);
-		printf("Пакетов %d из %d получено\n",k,g);
-		unsigned int kb_speed = (k * sizeof(struct msg));
-		kb_speed = kb_speed/1024;
-		printf("Скорость - %d kbyte/s\n",kb_speed/hope);
-		k = 0;
+		
+		TcpServerModel::printStatistic(hope, number_of_pockets, count_of_pockets);
+		
+		
+		count_of_pockets = 0;
 		clock_gettime(0,&time2);
         	close(s);
 		exit(0);
 	} 
 }
+
 
 
 
--- src/tcp_server_model.h
+++ src/tcp_server_model.h
@@ -30,4 +30,11 @@ class TcpServerModel : public Model {
 		void configure(struct configure_struct *);
 		void createTcpSocket(int &lis, struct sockaddr_in add); 
 		void acceptTcp(int lis,int &s, struct msg b);
+		void receiveInitData(long int &number_of_pockets,int &s, struct msg b);
+		void acceptClient(int lis, int &s);
+		void printSpeed(unsigned long int time, long int number_of_pockets);
+		void printTimeInSec(unsigned long int time);
+		void printNumberOfPockets(unsigned long int count ,unsigned long int number);
+		void printStatistic(long unsigned int, long int, long unsigned int);
+
 };
